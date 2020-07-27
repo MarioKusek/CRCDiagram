199d6dc3eb6433c4f4519396fc9af0325bcbd7ed
MarioKusek <mario.kusek@fer.hr>
2020-07-27 21:58:28 +0200

    Typo in class name

--- src/test/java/hr/fer/tel/crc/parser/ParseClassBodyTest.java
+++ src/test/java/hr/fer/tel/crc/parser/ParseClassBodyTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
-class ParseClassBodtTest {
+class ParseClassBodyTest {
 
   @Test
   void noClassBody() {
