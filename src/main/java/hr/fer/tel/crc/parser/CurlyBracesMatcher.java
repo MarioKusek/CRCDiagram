3397f943f9f4d237b1d78e72e62a2833ee9ac4a9
MarioKusek <mario.kusek@fer.hr>
2020-07-27 18:23:18 +0200

    Improved name of method for finding matching curly braces

--- src/main/java/hr/fer/tel/crc/parser/CurlyBracesMatcher.java
+++ src/main/java/hr/fer/tel/crc/parser/CurlyBracesMatcher.java
@@ -2,7 +2,7 @@ package hr.fer.tel.crc.parser;
 
 public class CurlyBracesMatcher {
 
-  public static int find(String text, int openCurlyBraceIndex) {
+  public static int findMatchingCurlyBrace(String text, int openCurlyBraceIndex) {
     if(text.length() == 0 || text.charAt(openCurlyBraceIndex) != '{')
       return -1;
 
--- src/test/java/hr/fer/tel/crc/parser/MatchingCurlyBracesTests.java
+++ src/test/java/hr/fer/tel/crc/parser/MatchingCurlyBracesTests.java
@@ -8,41 +8,41 @@ class MatchingCurlyBracesTests {
 
   @Test
   void onlyCurlyBraces() {
-    assertThat(CurlyBracesMatcher.find("{}",0)).isEqualTo(1);
+    assertThat(CurlyBracesMatcher.findMatchingCurlyBrace("{}",0)).isEqualTo(1);
   }
 
   @Test
   void emptyString_shouldReturnMinusOne() throws Exception {
-    assertThat(CurlyBracesMatcher.find("",0)).isEqualTo(-1);
+    assertThat(CurlyBracesMatcher.findMatchingCurlyBrace("",0)).isEqualTo(-1);
   }
 
   @Test
   void noBracesOnInputIndex_shouldReturnMinusOne() throws Exception {
-    assertThat(CurlyBracesMatcher.find(" ",0)).isEqualTo(-1);
+    assertThat(CurlyBracesMatcher.findMatchingCurlyBrace(" ",0)).isEqualTo(-1);
   }
 
   @Test
   void noClosingBrace_shouldReturnMinusOne() throws Exception {
-    assertThat(CurlyBracesMatcher.find("{",0)).isEqualTo(-1);
+    assertThat(CurlyBracesMatcher.findMatchingCurlyBrace("{",0)).isEqualTo(-1);
   }
 
   @Test
   void noOpeningBrace_shouldReturnMinusOne() throws Exception {
-    assertThat(CurlyBracesMatcher.find("}",0)).isEqualTo(-1);
+    assertThat(CurlyBracesMatcher.findMatchingCurlyBrace("}",0)).isEqualTo(-1);
   }
 
   @Test
   void curlyBracesWithContent() throws Exception {
-    assertThat(CurlyBracesMatcher.find("{ }",0)).isEqualTo(2);
+    assertThat(CurlyBracesMatcher.findMatchingCurlyBrace("{ }",0)).isEqualTo(2);
   }
 
   @Test
   void nestingCurlyBracesWithContent() throws Exception {
-    assertThat(CurlyBracesMatcher.find("{{ }}",0)).isEqualTo(4);
+    assertThat(CurlyBracesMatcher.findMatchingCurlyBrace("{{ }}",0)).isEqualTo(4);
   }
 
   @Test
   void nestingCurlyBracesWithContent2() throws Exception {
-    assertThat(CurlyBracesMatcher.find("{a{b}c{d{e}f}g}",6)).isEqualTo(12);
+    assertThat(CurlyBracesMatcher.findMatchingCurlyBrace("{a{b}c{d{e}f}g}",6)).isEqualTo(12);
   }
 }
