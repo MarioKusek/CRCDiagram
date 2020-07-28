9a1185983d783d92285d6ab358685da1046c6655
MarioKusek <mario.kusek@fer.hr>
2020-07-28 19:05:17 +0200

    Diagram with two classes with same name

--- src/test/java/hr/fer/tel/crc/DiagramTest.java
+++ src/test/java/hr/fer/tel/crc/DiagramTest.java
@@ -1,6 +1,7 @@
 package hr.fer.tel.crc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 
@@ -17,4 +18,14 @@ class DiagramTest {
     assertThat(diagram.getClassByKey("cl1")).isEqualTo(cl1);
     assertThat(diagram.getClassByKey("cl2")).isEqualTo(cl2);
   }
+
+  @Test
+  void twoClassesWithSameName() throws Exception {
+    Class cl1 = new Class("cl1");
+    Class cl2 = new Class("cl1");
+    assertThrows(RuntimeException.class, () -> {
+      new Diagram(List.of(cl1, cl2));
+    });
+  }
+
 }
