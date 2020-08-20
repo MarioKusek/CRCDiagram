4e02256882e3293fe6d99cef088b210242a75cc6
MarioKusek <mario.kusek@fer.hr>
2020-08-20 09:37:00 +0200

    Added private constructor

--- src/main/java/hr/fer/tel/crc/generator/DotToImageGenerator.java
+++ src/main/java/hr/fer/tel/crc/generator/DotToImageGenerator.java
@@ -10,38 +10,41 @@ import java.util.Map;
 
 public class DotToImageGenerator {
 
+  private DotToImageGenerator() {
+  }
+
   public static void generate(String graph, String outputFile, FileFormat fileFormat, String pathToDot)
       throws IOException, InterruptedException {
     ProcessBuilder builder;
-  
+
     String shell = System.getenv("SHELL");
     builder = new ProcessBuilder(shell, "-c", "dot -o" + outputFile + " -T " + fileFormat.getFormatText());
-  
+
     if(pathToDot != null) {
       Map<String, String> env = builder.environment();
       env.compute("PATH", (k, v) -> {
         return v + File.pathSeparator + pathToDot;
       });
     }
-  
+
     builder.redirectErrorStream(true);
     Process dotProcess = builder.start();
-  
+
     if(dotProcess.isAlive()) {
       OutputStream dotOutputStream = dotProcess.getOutputStream();
       dotOutputStream.write(graph.getBytes(StandardCharsets.UTF_8));
       dotOutputStream.close();
     }
-  
+
     BufferedReader reader = new BufferedReader(new InputStreamReader(dotProcess.getInputStream()));
     String line = reader.readLine();
     while (line != null) {
       System.out.println("Running Graphviz dot command: " + line);
       line = reader.readLine();
     }
-  
+
     dotProcess.waitFor();
-  
+
     if(dotProcess.exitValue() != 0)
       System.exit(dotProcess.exitValue());
   }
