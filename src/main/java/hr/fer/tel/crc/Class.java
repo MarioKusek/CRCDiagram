065c9cf47b56bcc58e5578e65e66eb3f1b57a5e1
MarioKusek <mario.kusek@fer.hr>
2020-08-20 09:35:46 +0200

    Added annotation for PMD to suppres warning

--- src/main/java/hr/fer/tel/crc/Class.java
+++ src/main/java/hr/fer/tel/crc/Class.java
@@ -14,6 +14,7 @@ import lombok.Singular;
 @Getter
 @Setter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@SuppressWarnings("PMD.BeanMembersShouldSerialize")
 public class Class {
 
   private final String name;
