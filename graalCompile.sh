998cac2964a631a5e8115c7153d62eb06aa47203
MarioKusek <mario.kusek@fer.hr>
2023-10-30 20:27:59 +0100

    Update versions

--- graalCompile.sh
+++ graalCompile.sh
@@ -1,11 +1,17 @@
 #!/bin/zsh
 
-# use sdk to change JVM
-export SDKMAN_DIR=/Users/mario/.sdkman
-echo "before sourcing"
-[[ -s "/Users/mario/.sdkman/bin/sdkman-init.sh" ]] && source "/Users/mario/.sdkman/bin/sdkman-init.sh"
-sdk use java 20.1.0.r11-grl
+# use sdk to change JVM - this is needed if graal is used directly (without gradle)
+#export SDKMAN_DIR=/Users/mario/.sdkman
+#echo "before sourcing"
+#[[ -s "/Users/mario/.sdkman/bin/sdkman-init.sh" ]] && source "/Users/mario/.sdkman/bin/sdkman-init.sh"
+#sdk use java 20.1.0.r11-grl
 
 ./gradlew build fatJar
-native-image -jar build/libs/CrcDiagram-all-0.0.1-SNAPSHOT.jar crcDiagram
-cp crcDiagram /Users/mario/Applications/bin/crcDiagram
+./gradlew nativeCompile
+# native-image -jar build/libs/CrcDiagram-all-0.0.1-SNAPSHOT.jar crcDiagram
+# make executable smaller by usin upx - can be installed by brew
+#rm -rf ./build/graal/crcDiagram_small
+#upx -7 ./build/graal/crcDiagram -o ./build/graal/crcDiagram_small
+
+# install to my path
+cp ./build/native/nativeCompile/crcDiagram /Users/mario/Applications/bin/crcDiagram
